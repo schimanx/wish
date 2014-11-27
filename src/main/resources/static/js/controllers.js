8d3317173fe0ca864592a7006d17e41347c93f72
schimanx <schiman@gmail.com>
2014-11-27 10:56:16 +0100

    Added sorting and search

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -3,6 +3,22 @@ var wishControllers = angular.module('wishControllers', []);
 wishControllers.controller('WishesCtrl', ['$scope', 'Wish',
     function ($scope, Wish) {
         $scope.wishes = Wish.query();
+        $scope.order = 'rating';
+        $scope.reverse = true;
+
+        $scope.ratingToClass = function(rating) {
+            if (rating > 80) {
+                return "progress-bar-danger";
+            } else if (rating > 60) {
+                return "progress-bar-warning";
+            } else if (rating > 40) {
+                return "progress-bar-success";
+            } else if (rating > 20) {
+                return "progress-bar-info";
+            } else {
+                return "";
+            }
+        }
     }]);
 
 wishControllers.controller('WishCtrl', ['$scope', '$routeParams', 'Wish',
