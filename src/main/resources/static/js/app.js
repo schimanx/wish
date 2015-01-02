b86080ce4d41657588a68ce58ec7689f69f63b0f
schimanx <schiman@gmail.com>
2015-01-02 23:51:22 +0100

    added wish detail

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -10,13 +10,32 @@ angular.module('wishApp', [
             $routeProvider.
                 when('/wishes', {
                     templateUrl: '/template/wishes.html',
-                    controller: 'WishesCtrl'
+                    controller: 'WishesController'
                 }).
                 when('/wish/:wishId', {
                     templateUrl: '/template/wish.html',
-                    controller: 'WishCtrl'
+                    controller: 'WishController'
+                }).
+                when('/wish', {
+                    templateUrl: '/template/wish.html',
+                    controller: 'WishController'
                 }).
                 otherwise({
                     redirectTo: '/wishes'
                 });
-        }]);
+        }])
+    .run(function($rootScope) {
+        $rootScope.ratingToClass = function (rating) {
+            if (rating > 80) {
+                return "progress-bar-danger";
+            } else if (rating > 60) {
+                return "progress-bar-warning";
+            } else if (rating > 40) {
+                return "progress-bar-success";
+            } else if (rating > 20) {
+                return "progress-bar-info";
+            } else {
+                return "";
+            }
+        };
+    });
--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,23 +1,9 @@
 angular.module('wishControllers', [])
-    .controller('WishesCtrl', ['$scope', 'Wish',
+    .controller('WishesController', ['$scope', 'Wish',
         function ($scope, Wish) {
             $scope.wishes = Wish.query();
             $scope.order = '-rating';
             $scope.reverse = false;
-
-            $scope.ratingToClass = function (rating) {
-                if (rating > 80) {
-                    return "progress-bar-danger";
-                } else if (rating > 60) {
-                    return "progress-bar-warning";
-                } else if (rating > 40) {
-                    return "progress-bar-success";
-                } else if (rating > 20) {
-                    return "progress-bar-info";
-                } else {
-                    return "";
-                }
-            };
 
             $scope.delete = function (item) {
                 Wish.delete({id: item.id}, function (success) {
@@ -26,7 +12,9 @@ angular.module('wishControllers', [])
                 });
             };
         }])
-    .controller('WishCtrl', ['$scope', '$routeParams', 'Wish',
+    .controller('WishController', ['$scope', '$routeParams', 'Wish',
         function ($scope, $routeParams, Wish) {
-            $scope.wish = Wish.get({id: $routeParams.wishId});
+            if ($routeParams.wishId != null) {
+                $scope.wish = Wish.get({id: $routeParams.wishId});
+            }
         }]);
