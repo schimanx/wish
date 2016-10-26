69a649e44be351a052f41abb202dc24387dddafb
schimanx <schiman@gmail.com>
2016-10-26 09:29:01 +0200

    update web security

--- src/main/java/cz/schiman/wish/Application.java
+++ src/main/java/cz/schiman/wish/Application.java
@@ -1,12 +1,13 @@
 package cz.schiman.wish;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@ComponentScan
 public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
